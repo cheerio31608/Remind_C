5517201f64cc65f48f83b6970b5f4cd29fbfddde
KIMSHINWOO <swkim9985@naver.com>
2021-04-01 15:32:02 +0900

    first

---
+++ Remind_C/bubble.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define SIZE 5
+
+int main(void)
+{
+    int i, k;
+    int list[SIZE] = { 16, 7, 9, 1, 3};
+
+    for(k = 0; k < 5; k++){
+        for(i = 0; i < 4-k; i++){
+            if(list[i] < list[i+1]){
+                int tmp = list[i];
+                list[i] = list[i+1];
+                list[i+1] = tmp;
+            }
+        }
+    }
+
+    for(i = 0; i < 5; i++){
+        printf("%d ", list[i]);
+    }
+
+
+    return 0;
+}
